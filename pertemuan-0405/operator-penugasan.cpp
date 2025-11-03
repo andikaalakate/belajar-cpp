eb499c12b3474e0d272340ea156b77f5e88632a1
Andika Alakate <andikaalakate@gmail.com>
2025-11-03 11:48:02 +0700

    Update: Tambahan Operator Penugasan

--- pertemuan-0405/operator-penugasan.cpp
+++ pertemuan-0405/operator-penugasan.cpp
@@ -3,13 +3,43 @@ using namespace std;
 
 int main() {
     int x = 10;
-    cout << "Nilai X saat ini = " << x << endl;
+
+    cout << "--- Operator Penugasan (Assignment) ---\n";
+
+    cout << endl;
     
-    x += 16;
+    cout << "Nilai X awal = " << x << endl;
     cout << "Nilai X saat ini = " << x << endl;
+
+    cout << endl;
     
     x += 5;
+    cout << "x += 5; => " << x << endl;
     cout << "Nilai X saat ini = " << x << endl;
-    
+
+    cout << endl;
+
+    x -= 3;
+    cout << "x -= 3; => " << x << endl;
+    cout << "Nilai X saat ini = " << x << endl;
+
+    cout << endl;
+
+    x *= 2;
+    cout << "x *= 2; => " << x << endl;
+    cout << "Nilai X saat ini = " << x << endl;
+
+    cout << endl;
+
+    x /= 4;
+    cout << "x /= 4; => " << x << endl;
+    cout << "Nilai X saat ini = " << x << endl;
+
+    cout << endl;
+
+    x %= 4;
+    cout << "x %= 4; => " << x << endl;
+    cout << "Nilai X saat ini = " << x << endl;
+
     return 0;
 }
