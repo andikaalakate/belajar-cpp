ba269d380cbe76aa05ad3bdaf7645cb676975532
Andika Alakate <andikaalakate@gmail.com>
2026-05-18 12:58:46 +0700

    Anu

--- semester-2/pertemuan-1112/kasus-ketiga.cpp
+++ semester-2/pertemuan-1112/kasus-ketiga.cpp
@@ -1,24 +1,26 @@
 ﻿#include <iostream>
 #include <vector>
 using namespace std;
-int main() {
-     // Deklarasi Vector
-     vector<string> kendaraan = {"Mobil", "Motor", "Sepeda"};
+int main()
+{
+  // Deklarasi Vector
+  vector<string> kendaraan = {"Mobil", "Motor", "Sepeda"};
 
-     // Menambah elemen di belakang
-     kendaraan.push_back("Truk");
-  
-     // Mengakses dan mengubah elemen
-    kendaraan.at(0) = "Bus";
+  // Menambah elemen di belakang
+  kendaraan.push_back("Truk");
 
-    // Menghapus elemen terakhir
-    kendaraan.pop_back();
+  // Mengakses dan mengubah elemen
+  kendaraan.at(0) = "Bus";
 
-    cout << "Isi Vector saat ini:\n";
-    for(string k : kendaraan) {
-        cout << "- " << k << "\n";
- }
- 
-   cout << "Jumlah elemen: " << kendaraan.size() << endl;
- return 0;
+  // Menghapus elemen terakhir
+  kendaraan.pop_back();
+
+  cout << "Isi Vector saat ini:\n";
+  for (string k : kendaraan)
+  {
+    cout << "- " << k << "\n";
+  }
+
+  cout << "Jumlah elemen: " << kendaraan.size() << endl;
+  return 0;
 }
